62da244a767a71efc285d9050cfcb16b9db2f4ca
risna <672020237@student.uksw.edu>
2023-12-29 14:47:13 +0700

    router 4

--- src/App.js
+++ src/App.js
@@ -4,17 +4,17 @@ import Home from "./routes/Home";
 import About from "./routes/About";
 import Project from "./routes/Project";
 import Contact from "./routes/Contact";
-import { Route, Router } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <Router basename="/portofolio">
+      <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/project" element={<Project />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
-      </Router>
+      </Routes>
     </>
   );
 }
